4b5c43537dc48a4a5d1e0654f6bffffed4d1daeb
ngoquocdat <ngoquocdat093@gmail.com>
2023-09-27 15:44:40 +0700

    RealDeal fixed issue

--- src/Components/News/newsList.tsx
+++ src/Components/News/newsList.tsx
@@ -15,43 +15,44 @@ export default function NewsList(props: INewsList) {
 
   return (
     <div className="real-estate-news-wrapper">
-      {counter.map((num, index) => (
-        <div className={`news-item ${num}`}>
-          <Box
-            className="image"
-            component="img"
-            src={news[index]?.featured_image}
-          />
-          <div className="info">
+      {news?.length &&
+        counter.map((num, index) => (
+          <div className={`news-item ${num}`}>
             <Box
-              className="title"
-              sx={{ fontSize: `${titleSize || 24}px`, fontWeight: 700 }}
-            >
-              {news[index]?.title}
-            </Box>
-            <br />
-            <b>Diện tích: </b>120 m2
-            <br />
-            <b>Tình trạng: </b>bàn giao hoàn chỉnh Quý 3, 2023
-            <br />
-          </div>
-          <div className="description">
-            {news[index]?.excerpt}
-            <div>
-              <Button
-                className="join-to-room-button rd-buttons contained-button"
-                variant="contained"
-                onClick={(evt?: any) => {
-                  selectedNews.setSelectedNews(news[index]);
-                  toggleDialog(true);
-                }}
+              className="image"
+              component="img"
+              src={news[index]?.featured_image}
+            />
+            <div className="info">
+              <Box
+                className="title"
+                sx={{ fontSize: `${titleSize || 24}px`, fontWeight: 700 }}
               >
-                Xem chi tiết tin tức
-              </Button>
+                {news[index]?.title}
+              </Box>
+              <br />
+              <b>Diện tích: </b>120 m2
+              <br />
+              <b>Tình trạng: </b>bàn giao hoàn chỉnh Quý 3, 2023
+              <br />
+            </div>
+            <div className="description">
+              {news[index]?.excerpt}
+              <div>
+                <Button
+                  className="join-to-room-button rd-buttons contained-button"
+                  variant="contained"
+                  onClick={(evt?: any) => {
+                    selectedNews.setSelectedNews(news[index]);
+                    toggleDialog(true);
+                  }}
+                >
+                  Xem chi tiết tin tức
+                </Button>
+              </div>
             </div>
           </div>
-        </div>
-      ))}
+        ))}
     </div>
   );
 }
