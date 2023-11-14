7569f9708da263cea7fbc57ea2f6f54e7e4f244c
ngoquocdat <ngoquocdat093@gmail.com>
2023-11-14 10:48:14 +0700

    RealDeal: Resolve conflict file src/Components/chatRoom/Services/ChatRoomservice.tsx

--- src/Components/ChatRoom/Services/ChatRoomService.tsx
+++ src/Components/ChatRoom/Services/ChatRoomService.tsx
@@ -12,33 +12,29 @@ export default class ChatRoomService {
       } from ${roomId}. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor.`;
       const avatar = `https://th.bing.com/th/id/R.588e9f4570ef5ddee0ef2f0c3eb4d237?rik=VDy%2fh%2ffN3XJm6Q&amp;pid=ImgRaw&amp;r=0`;
 
-export default class ChatRoomService
-{
-    getMessages = (roomId: string): Message[] => {
-        return Array.from({ length: 30 }, (_, item) => {
-          const id = `${item + 1}`;
-          const isAgency = item % 2 === 0;
-          const date = new Date().toLocaleString();
-          const title = `Message Title ${item + 1}`;
-          const text = `This is a longer message number ${item + 1} from ${roomId}. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor.`;
-          const avatar = `https://th.bing.com/th/id/R.588e9f4570ef5ddee0ef2f0c3eb4d237?rik=VDy%2fh%2ffN3XJm6Q&amp;pid=ImgRaw&amp;r=0`
-      
-          return { id, roomId, isAgency, date, title, text, avatar };
-        }).filter(message => message.roomId === roomId);
-      };
-    
-    getRooms = (): Room[] =>
-    {
-        return getRooms
-    } 
+      return { id, roomId, isAgency, date, title, text, avatar };
+    }).filter((message) => message.roomId === roomId);
+  };
 
-    getRoomId(RealEstatesId: number, RealEstatesTitle: string, RealEstatesLocation: string, CreateTIme: string)
-    {
-        return RealEstatesId.toString() + RealEstatesTitle + RealEstatesLocation + CreateTIme
-    }
+  getRooms = (): Room[] => {
+    return getRooms;
+  };
 
-    getRoomName(RealEstatesTitle: string, CreateTIme: string)
-    {
-        return RealEstatesTitle + CreateTIme
-    }
+  getRoomId(
+    RealEstatesId: number,
+    RealEstatesTitle: string,
+    RealEstatesLocation: string,
+    CreateTIme: string
+  ) {
+    return (
+      RealEstatesId.toString() +
+      RealEstatesTitle +
+      RealEstatesLocation +
+      CreateTIme
+    );
+  }
+
+  getRoomName(RealEstatesTitle: string, CreateTIme: string) {
+    return RealEstatesTitle + CreateTIme;
+  }
 }
